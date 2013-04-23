447227f65c8d7e891f05289239aee9c2be8b3fcd
michael99man <michael99man.bj@gmail.com>
2013-04-23 21:22:09 +0800

    Added framework for updating moods

--- src/com/github/michael99man/SkypeMoodScheduler/MainFrame.java
+++ src/com/github/michael99man/SkypeMoodScheduler/MainFrame.java
@@ -2,7 +2,11 @@ package com.github.michael99man.SkypeMoodScheduler;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.io.IOException;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -10,10 +14,11 @@ import javax.swing.border.EmptyBorder;
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = -487046159617990173L;
-	
-	
+
 	private JPanel contentPane;
 
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -40,6 +45,28 @@ public class MainFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		
+		System.out.println("Window Launch");
+		
+		
+		changeMood("Hi! This is an attempt to change my mood message using Applescript within Shell within Java! IKR");
 
+				
+
+	}
+	
+	private static final ScriptEngineManager sem = new ScriptEngineManager();
+	private static final ScriptEngine engine = sem.getEngineByName("AppleScript");
+	
+	public static void changeMood(String in){
+		
+		String script = "tell application \"Skype\" to send command \"SET PROFILE MOOD_TEXT " + in + "\" script name \"MoodChanger\"";
+		
+		try {
+			engine.eval(script);
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
